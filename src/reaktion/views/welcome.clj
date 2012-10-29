7c5bab7d0125f9a457b27750c0a6d430217dc394
Jim Barritt <jim@planet-ix.com>
2012-10-29 20:42:59 +0000

    [jmdb] Initial commit with blank noir project

---
+++ src/reaktion/views/welcome.clj
@@ -0,0 +1,8 @@
+(ns reaktion.views.welcome
+  (:require [reaktion.views.common :as common]
+            [noir.content.getting-started])
+  (:use [noir.core :only [defpage]]))
+
+(defpage "/welcome" []
+         (common/layout
+           [:p "Welcome to reaktion"]))
