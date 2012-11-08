c11df0d8654e5a8434511388b0e254bef12ec54a
Jim Barritt <jim@planet-ix.com>
2012-11-08 20:55:48 +0000

    [jmdb] Extracted the talk reaktion so that it is parameterised with data

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject reaktion "0.1.0-SNAPSHOT"
             :description "FIXME: write this!"
             :dependencies [[org.clojure/clojure "1.4.0"]
-                           [congomongo "0.1.5-SNAPSHOT"]
+                           [congomongo "0.3.3"]
                            [noir "1.3.0-beta3"]]
             :main reaktion.server)
 
--- src/reaktion/views/index.clj
+++ src/reaktion/views/index.clj
@@ -39,42 +39,39 @@
    [:label "Enter any comments" [:textarea.comments]]])
 
 (def talk-data
-  {:der42323234 
-    {:is :talk
+   
+  [{:is :talk
      :title "The Catcher in the code"
      :speaker "Pavel Badenski"
-     :speaker-img "https://secure.gravatar.com/avatar/5c934bb3826efd22dcb43894b817153a?s=400&d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png"}
-   :saer3224234123 
+     :speaker-img "https://secure.gravatar.com/avatar/5c934bb3826efd22dcb43894b817153a?s=400&d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png"},
+    
     {:is :talk
      :title "Programming in the large"
      :speaker "James Lewis"
-     :speaker-img "https://trello-avatars.s3.amazonaws.com/6b2b6c28c5a69c16844d55efe7113bdc/30.png"}})
+     :speaker-img "https://trello-avatars.s3.amazonaws.com/6b2b6c28c5a69c16844d55efe7113bdc/30.png"}])
 
 
-(defpage "/" []
+(defpartial reakt-to-a-talk [{:keys [id title speaker speaker-img]}]
+  
+  [:h1 "Let's get your reaktion"]
+  [:div.talk
+   [:form {:action (format "/talks/%s" id)}
+    [:h2.title title]
+    [:img.speaker {:src speaker-img
+                   :alt speaker}]
+    [:p.speaker speaker]
+    
+    [:div.ratings
+     [:h2 "Your feedback..."]
+     (rating-question "overall-impression")
+     (rating-question "presentation-style")
+     (rating-question "technical-interest")
+     (rating-question "slidewate-quality")
+     (rating-question "clarity-of-communication")
+     (comment-box "Enter any comments")      
+     [:input.btn {:type "submit" :value "REAKT!"}]]]])
+
+(defpage [:get "/"] {}
   (common/layout
    {:title "reaktion - home"}
-   [:h1 "Let's get your reaktion"]
-   [:div.talk
-    [:form {:action "/"}
-     [:h2.title "Talk: The Catcher In the code"]
-     [:img.speaker {:src "https://secure.gravatar.com/avatar/5c934bb3826efd22dcb43894b817153a?s=400&d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png"
-                    :alt "Pawel Badenski"}]
-     [:p.speaker "Pavel Badenski"]
-     [:p "If you select lower than 3 bytes (will be zeroes and ones) you will need to enter a comment."]     
-     ;; [:div.reaktion      
-     ;;  (choice-image "reaktion" "zzz" "/img/sleep.jpeg" "Zzzzz")
-     ;;  (choice-image "reaktion" "er?" "/img/confused.jpeg" "Er...?")
-     ;;  (choice-image "reaktion" "hmm" "/img/bored.jpeg" "Hmmm...?")
-     ;;  (choice-image "reaktion" "ok" "/img/ok.jpeg" "Ok")
-     ;;  (choice-image "reaktion" "wow" "/img/monumental.jpeg" "Monumental!")]
-
-     [:div.ratings
-      [:h2 "Your feedback..."]
-      (rating-question "overall-impression")
-      (rating-question "presentation-style")
-      (rating-question "technical-interest")
-      (rating-question "slidewate-quality")
-      (rating-question "clarity-of-communication")
-      (comment-box "Enter any comments")      
-      [:input.btn {:type "submit" :value "REAKT!"}]]]]))
+   (reakt-to-a-talk (first talk-data))))
