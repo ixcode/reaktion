b2c21eb33843737f69ccfcdb638003f6d3552e4d
Seema Satish <seema.satish@digital.cabinet-office.com>
2012-11-08 15:40:57 +0000

    added congomongo to talk to mongodb

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject reaktion "0.1.0-SNAPSHOT"
             :description "FIXME: write this!"
             :dependencies [[org.clojure/clojure "1.4.0"]
+                           [congomongo "0.1.5-SNAPSHOT"]
                            [noir "1.3.0-beta3"]]
             :main reaktion.server)
 
