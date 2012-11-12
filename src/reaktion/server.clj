99290d6044601af90c41c262658d5aba63078096
Jim Barritt <jim@planet-ix.com>
2012-11-12 22:46:53 +0000

    [jmdb] getting debugging to work

--- src/reaktion/server.clj
+++ src/reaktion/server.clj
@@ -8,8 +8,8 @@
   (let [mode (keyword (or (first m) :dev ))
         port (Integer. (get (System/getenv) "PORT" "8080"))]
     (println "Hello!")
-    (println (format "PORT: %d" port))
-    (println (format "MONGOHQ_URL: %d" (System/getenv "MONGOHQ_URL")))
+    (println (format "PORT: %s" port))
+    (println (format "MONGOHQ_URL: %s" (System/getenv "MONGOHQ_URL")))
 ;;    (mongo/connect-mongo!)
     (server/start port {:mode mode
                         :ns 'reaktion})))
