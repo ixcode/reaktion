28844abc607786fcd461344553ff505c433e662e
Jim Barritt <jim@planet-ix.com>
2012-11-14 08:50:47 +0000

    [jmdb] Adding enlive to the mix!

---
+++ src/reaktion/views/enlive_helpers.clj
@@ -0,0 +1,25 @@
+(ns reaktion.views.enlive_helpers
+  (:require [net.cgrand.enlive-html :as html]
+            clojure.pprint))
+
+(defn render [template]
+  (apply str template))
+
+(defmacro maybe-substitute
+  ([expr] `(if-let [x# ~expr] (html/substitute x#) identity))
+  ([expr & exprs] `(maybe-substitute (or ~expr ~@exprs))))
+
+(defmacro maybe-content
+  ([expr] `(if-let [x# ~expr] (html/content x#) identity))
+  ([expr & exprs] `(maybe-content (or ~expr ~@exprs))))
+
+(defmacro meta-tag [name content]
+    [(and (html/has [:meta]) (html/attr-has :name name))] (html/set-attr :content content))
+
+(html/deftemplate layout "reaktion/views/layout.html"
+  [{:keys [title content]}]
+  [:title]  (maybe-content title)
+  ;;(meta-tag "description" title) Need help!
+  [(and (html/has [:meta]) (html/attr-has :name "description"))] (html/set-attr :content title)
+  [:div.main] (maybe-content content))
+
